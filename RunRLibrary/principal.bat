b92dafe060a8c65613109eba8764ad66db433a47
manromero <miguelangelnunezromero@gmail.com>
2017-11-07 14:19:22 +0100

    scripts ejemplo para ejecutar aplicacion

---
+++ RunRLibrary/principal.bat
@@ -0,0 +1,4 @@
+echo Empieza la carga del programa....
+start subrutina1.bat
+start subrutina2.bat
+cscript MessageBox.vbs "Espere mientras el programa se inicia correctamente"
---
+++ RunRLibrary/subrutina2.bat
@@ -0,0 +1,4 @@
+echo Cargando subrutina 2...
+cd "C:\Users\manromero\RLibrary\RLibraryNG"
+ng serve
+pause
