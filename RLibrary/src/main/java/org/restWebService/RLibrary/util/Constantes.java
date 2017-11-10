e473374c807b0f251fd86ff47c0219625f86fd2b
manromero <miguelangelnunezromero@gmail.com>
2017-11-10 10:02:34 +0100

    allowed origins cors

--- RLibrary/src/main/java/org/restWebService/RLibrary/util/Constantes.java
+++ RLibrary/src/main/java/org/restWebService/RLibrary/util/Constantes.java
@@ -2,6 +2,8 @@ package org.restWebService.RLibrary.util;
 
 public class Constantes {
 	
+	public static final String URL_CLIENT = "http://localhost:4200";
+	
 	public static final String S_UPDATE = "update";
 	public static final String S_TRUE = "true";
 	public static final String S_FALSE = "true";
